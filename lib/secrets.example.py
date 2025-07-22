b9780abe1db0757ab3993b67c3d7f845dd9c87a6
Fernando Rios <zoidy@users.noreply.github.com>
2025-07-22 15:57:46 -0400

    Add env variables for board ID and list ID for Trello API calls

--- lib/secrets.example.py
+++ lib/secrets.example.py
@@ -12,6 +12,8 @@ api_figshare_token = ''
 api_trello_url_base = ''
 api_trello_key = ''
 api_trello_token = ''
+trello_board_id = ''
+trello_publishedlist_id = ''
 
 # Google sheet
 gsheets_dashboard_post_url = ''
