76fd5209b09b8a0a49d810105b35ffdb162ae56c
Fernando Rios <zoidy@users.noreply.github.com>
2025-07-22 23:19:59 -0400

    Add total items and time per curator to curator report

--- packages/trello_reports/run/curator_report.py
+++ packages/trello_reports/run/curator_report.py
@@ -5,8 +5,10 @@
 # Author: Fernando Rios
 
 import sys
+import traceback
 import requests
 import simplejson as json
+from ast import literal_eval
 from datetime import datetime
 from os import environ
 from trello import TrelloClient
@@ -21,26 +23,57 @@ def run(args):
     # Optionally writes a CSV report to file and returns a json array of objects with the data that was written.
 
     print('Getting curator stats from Trello')
-    curators = []
+    curators = {}
 
     tc = TrelloClient(api_key=environ['API_TRELLO_KEY'], api_secret=environ['API_TRELLO_TOKEN'])
     
     try:
         board = tc.get_board(environ['TRELLO_BOARD_ID'])
+        boardmembers = board.all_members()
         published_list = board.get_list(environ['TRELLO_PUBLISHEDLIST_ID'])
         cards = published_list.list_cards()
         for card in cards:
             print(card.name)
             for field in card.custom_fields:
                 print(f'    {field.name}: {field.value}')
+                if 'reviewer_' in field.name:
+                    if field.value not in curators:
+                        # Add a new curator to the list if they're not already there
+                        # The value of the reviewer_x custom field in trello must be a trello username
+                        curators[field.value] = {'id': '', 'total_items': 0, 'total_time': 0,
+                                                 'easy_items': 0, 'easy_time': 0, 'med_items': 0, 'med_time': 0,'hard_items': 0, 'hard_time': 0,
+                                                 '3M_items' :0, '3M_time': 0}
+                        for member in boardmembers:
+                            # Extract the id from the username
+                            if member.username == field.value:
+                                curators[field.value]['id'] = member.id
+
+                    curators[field.value]['total_items'] += 1
+            
+            print()
+            for plugin in card.plugin_data:
+                if plugin['idPlugin'] == '5c592ae4d74ac4407f4e3af3':
+                    # Get the recorded time from the Activity powerup (convert from milliseconds to seconds)
+                    powerup_timedata = literal_eval(plugin['value'])['users']
+                    for username, curator in curators.items():
+                        if curator['id'] in powerup_timedata:
+                            curator['total_time'] += int(powerup_timedata[curator['id']]['time'])/1000
+                            
+                    print(f'    {powerup_timedata}')
+                
     except Exception as e:
-        print(f'Error getting board info: {e}')
+        tb_list = traceback.extract_tb(sys.exc_info()[2])
+        line_number = tb_list[-1][1]
+        print(f'Error getting board info: {e}, line {line_number}')
+        return {}
+
+    print('Curation stats for items published since Jan 1, 2025')
 
     outfile = None
     if args.outfile:
         outfile = f.get_report_outfile(args.outfile, 'curators')
         outfile.write(
-            f'id,total_items,total_time ({args.units}),'
+            f'username,total_items,total_time ({args.units}),'
             + f'easy_items,easy_time ({args.units}),med_items,med_time ({args.units}),hard_items,hard_time ({args.units}),'
             + f'3M_items,3M_time ({args.units}),6M_items,6M_time ({args.units}),'
             + f'1Y_items,1Y_time ({args.units}),2Y_items,2Y_time ({args.units}),'
@@ -51,27 +84,29 @@ def run(args):
             + f'3M_hard_items,3M_hard_time ({args.units}),6M_hard_items,6M_hard_time ({args.units})'
             + f'1Y_hard_items,1Y_hard_time ({args.units}),2Y_hard_items,2Y_hard_time ({args.units})\n')
     else:
-        print(f'id\t,total_items\t,total_time ({args.units})')
+        print(f'username \t total_items \t total_time ({args.units})')
 
-    # for article in articles:
-        # if outfile:
-            # s = '{0},{1},{2},{3},{4},{11},{5},{6},{7},{8},{9},{10},{12}'
-        # else:
-            # s = '{0}\t,{1}\t,{2}\t,{3}\t,{4}\t,{5}'
+    total_time = 0
+    total_items = 0
 
-        # s = s.format(article['id'], article['version'], article['totalfilesize'],
-                     # '"' + article['title'].replace('"', '""') + '"',
-                     # article['type'],
-                     # article['published_date'], article['modified_date'], article['embargo_date'], article['embargo_type'],
-                     # article['embargo_options_type'], article['is_embargoed'], article['is_public'], article['report_date'])
+    for username, curator in curators.items():
+        if outfile:
+            pass
+        else:
+            s = '{0} \t {1} \t\t {2}'
+            
+        s = s.format(username, curator['total_items'], f.format_duration(str(curator['total_time'])+'s', args.units))
+        
+        total_time += curator['total_time']
+        total_items += curator['total_items']
 
-        # if outfile:
-            # outfile.write(s + '\n')
-        # elif not args.sync_to_dashboard:
-            # print(s)
+        if outfile:
+            outfile.write(s + '\n')
+        elif not args.sync_to_dashboard:
+            print(s)
 
-    #print(f'Totals (hours):\t\t{total_usage}')
-    #print(f'Totals ({args.units}):\t\t{f.format_bytes(total_usage, args.units)}')
+    print(f'Total curation hours:\t\t\t\t{f.format_duration(str(total_time)+'s', 'H')}')
+    print(f'Avg items per curator (primary or secondary):\t{total_items / len(curators)}')
 
     if outfile:
         outfile.close()
